01e70722978d246295909b4dd701119c64af141e
Emanuel <emanuelzsmj@gmail.com>
2021-02-22 20:29:35 -0300

    Regex para senhas / Validando datas

---
+++ js/validacao.js
@@ -0,0 +1,22 @@
+const dataNascimento = document.querySelector('#nascimento')
+
+dataNascimento.addEventListener('blur', (event) => {
+    validaDataNascimento(event.target)
+})
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+    maioQue18(dataRecebida)
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Voce deve ser maior que 18 anos para se cadastrar'
+    }
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
